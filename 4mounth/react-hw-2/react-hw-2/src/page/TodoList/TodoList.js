5c042e43a21b2405d0e49d5a8525f927709c2ff2
Faridekhalili <khalili.f@northeastern.edu>
2024-03-05 15:28:29 -0500

    Add useRef hw2.

--- 4mounth/react-hw-2/react-hw-2/src/page/TodoList/TodoList.js
+++ 4mounth/react-hw-2/react-hw-2/src/page/TodoList/TodoList.js
@@ -1,23 +1,23 @@
 import classes from './todoList.module.css'
 import Button from "../../components/Button/Button"
 import Modal from "../../components/Modal/Modal"
-import { useState } from "react"
+import { useState, useRef } from "react" // Import useRef
 import List from "../../components/List/List"
 import Input from "../../components/Input/Input"
-const TodoList = () => {
 
+const TodoList = () => {
     const [state, setState] = useState(false)
-    const [newTask, setNewTask] = useState('Добавить')
-    const [search, setSearch] = useState('Поиск')
+    const newTaskRef = useRef('Добавить') // Use useRef for newTask
+    const searchRef = useRef('Поиск') // Use useRef for search
 
     const handleShow = () => {
         setState(!state)
     }
     const handleNewTask = (text) => {
-        setNewTask(text)
+        newTaskRef.current = text // Update useRef value
     }
     const handleSearch = (text) => {
-        setSearch(text)
+        searchRef.current = text // Update useRef value
     }
 
     const links = [
@@ -47,19 +47,18 @@ const TodoList = () => {
                 state && <Modal handleShow={handleShow}>
                     <button className={classes.close} onClick={handleShow}>X</button>
 
-                    <h2>{search}</h2>
+                    <h2>{searchRef.current}</h2>
                     <input onChange={(event) => handleSearch(event.target.value)} className={classes.input} type="text" placeholder='search' />
                     <span></span>
                     <Button>Поиск</Button>
 
-                    <h2>{newTask}</h2>
-                    <Input handleNewTask={handleNewTask} />
+                    <h2>{newTaskRef.current}</h2> 
+                    <Input handleNewTask={(text) => handleNewTask(text)} /> 
                     <span></span>
                     <Button>Добавить</Button>
                 </Modal>
             }
         </div>
     )
-
 }
 export default TodoList
